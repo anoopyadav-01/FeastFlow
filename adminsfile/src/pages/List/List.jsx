6810aec9ab7840ee5b4d690e9a49135f551344fe
anoopyadav-01 <anoopyadav7977195135@gmail.com>
2026-07-12 00:46:48 +0530

    Add files via upload

---
+++ adminsfile/src/pages/List/List.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import "./List.css";
+import axios from "axios";
+import { toast } from "react-toastify";
+const List = ({ url }) => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    console.log(response.data);
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  const removeFood = async (foodId) => {
+    const response = await axios.post(`${url}/api/food/remove`, { id: foodId });
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+  const myStyles = {
+    marginTop: "30px",
+    marginLeft: "55px",
+  };
+
+  const pstyle = {
+    color: "black",
+    fontWeigth: "20px",
+  };
+  return (
+    <div style={myStyles} className="list add flex-col">
+      <p style={pstyle}>All Food List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img src={`${url}/images/${item.image}`} alt={item.name} />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>{item.price}</p>
+              <p onClick={() => removeFood(item._id)} className="cursor">
+                {" "}
+                X
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
